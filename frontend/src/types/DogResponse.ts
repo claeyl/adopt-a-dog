54a82049be70c7d0da722e110c4bf856d392101f
Calley Chai <annyi.chai@gmail.com>
2025-12-08 21:51:14 +0800

    feat: type for dog response

---
+++ frontend/src/types/DogResponse.ts
@@ -0,0 +1,12 @@
+export interface DogResponse {
+  id: number
+  name: string
+  gender: string
+  age: number
+  breed: string
+  size: string
+  weight: number
+  adoptionFee: number
+  tags: string[]
+  description: string
+}
