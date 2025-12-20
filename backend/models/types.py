6607ccae8639793b1d25bcd6a79e64d9d9ffedd8
Calley Chai <annyi.chai@gmail.com>
2025-12-20 22:43:50 +0800

    feat: adding literal types

--- backend/models/types.py
+++ backend/models/types.py
@@ -5,6 +5,9 @@ Gender = Literal["male", "female"]
 Size = Literal["small", "medium", "large"]
 Children = Literal["any", "older", "teenager", "none"]
 
+ComparisonOperator = Literal["equal", "not_equal", "less_than", "less_or_equal", "greater_than", "greater_or_equal"]
+LogicalOperator = Literal["or", "and"]
+
 class Tribool(Enum):
   TRUE = 0
   FALSE = 1
