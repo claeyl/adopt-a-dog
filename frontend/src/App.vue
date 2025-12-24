5883ed80b31112426751e8ccc8b307ccf0478515
Calley Chai <annyi.chai@gmail.com>
2025-12-24 22:22:12 +0800

    feat: readd suggestions

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -9,9 +9,9 @@
     </div>
     <div v-else class="results">
       <p class="results__summary">
-        Here are the dogs matching your query: {{ matchingDogsSummary }}
+        {{ matchingDogsSummary }}
       </p>
-      <DogCard v-for="dog in matchingDogs" :key="dog.id" :dog="dog" />
+      <DogCards :dogs="matchingDogs" />
     </div>
     <div class="search">
       <SearchBox
@@ -20,8 +20,8 @@
         :disabled="loading"
         @search="handleQuerySubmit"
       />
-      <!-- <Suggestions v-if="matching !== null" @suggestion-click="handleSuggestionClick" /> -->
-      <div class="search__error" v-if="error !== null">{{ error }}</div>
+      <Suggestions v-if="matchingDogs.length === 0" @suggestion-click="handleSuggestionClick" />
+      <div class="search__error" v-if="error.length > 0">{{ error }}</div>
     </div>
   </main>
 </template>
@@ -30,13 +30,14 @@
 import { ref } from 'vue'
 import SearchBox from './components/SearchBox.vue'
 import Suggestions from './components/Suggestions.vue'
-import DogCard from './components/DogCard.vue'
+import DogCards from './components/carousel/ResultsCarousel.vue'
 
 import data from './fake/fake_results.json'
+import type { DogResponse } from './types/DogResponse'
 
 const query = ref('')
 const loading = ref(false)
-const error = ref(null)
+const error = ref('')
 
 const matchingDogsSummary = ref('')
 const matchingDogs = ref<DogResponse[]>([])
@@ -47,6 +48,7 @@ const handleSuggestionClick = (content: string) => {
 
 const handleQuerySubmit = async () => {
   loading.value = true
+  matchingDogs.value = []
   matchingDogsSummary.value = query.value
   try {
     const response = await fetch(`${import.meta.env.VITE_BACKEND_API_URL}/query`, {
@@ -58,8 +60,8 @@ const handleQuerySubmit = async () => {
     matchingDogs.value = json.results
     // matchingDogs.value = data.results // for testing
   } catch (err) {
-    console.log(err)
-    error.value = err
+    console.error(err)
+    error.value = String(err)
   } finally {
     query.value = ''
     loading.value = false
@@ -72,7 +74,7 @@ main {
   display: flex;
   flex-direction: column;
   gap: 3rem;
-  width: 90%;
+  width: min(95%, max-content);
 }
 
 @media (min-width: 640px) {
