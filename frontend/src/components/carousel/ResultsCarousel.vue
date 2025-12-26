901a99acd2b384e0ba6bb90184297f29b5d2206e
Calley Chai <annyi.chai@gmail.com>
2025-12-26 17:41:32 +0800

    refactor: rearrange nav buttons in carousel

--- frontend/src/components/carousel/ResultsCarousel.vue
+++ frontend/src/components/carousel/ResultsCarousel.vue
@@ -1,12 +1,5 @@
 <template>
   <div class="carousel" role="carousel">
-    <button type="button" ariaLabel="previous" class="carousel__nav-btn prev" @click="onClickPrev">
-      <i class="pi pi-chevron-left"></i>
-    </button>
-    <button type="button" ariaLabel="next" class="carousel__nav-btn next" @click="onClickNext">
-      <i class="pi pi-chevron-right"></i>
-    </button>
-
     <div class="carousel__track">
       <div v-for="(dog, index) in dogs" :key="dog.id" v-show="index === currentIndex">
         <DogCard :dog="dog" />
@@ -14,6 +7,10 @@
     </div>
 
     <div class="carousel__nav-dots">
+      <button type="button" ariaLabel="previous" class="carousel__nav-btn" @click="onClickPrev">
+        <i class="pi pi-caret-left"></i>
+      </button>
+
       <button
         v-for="(dog, idx) in dogs"
         :key="dog.id"
@@ -22,6 +19,10 @@
         aria-label="Go to slide"
         class="carousel__dot"
       ></button>
+
+      <button type="button" ariaLabel="next" class="carousel__nav-btn" @click="onClickNext">
+        <i class="pi pi-caret-right"></i>
+      </button>
     </div>
   </div>
 </template>
@@ -61,35 +62,22 @@ const onClickNext = () => {
 
 .carousel__nav-btn {
   border: none;
-  padding: 1.05rem;
-  font-size: 1.25rem;
-  aspect-ratio: 1 / 1;
   cursor: pointer;
   border-radius: 50%;
-
-  background-color: var(--color-primary);
-  transition: background-color ease 0.5s;
-
-  position: absolute;
-}
-
-.carousel__nav-btn.prev {
-  left: 0%;
-  top: 50%;
-}
-
-.carousel__nav-btn.next {
-  right: 0%;
-  top: 50%;
+  color: var(--color-primary);
+  background-color: transparent;
+  font-size: 1.25rem;
+  outline: none;
 }
 
 .carousel__nav-btn:hover {
-  background-color: var(--color-primary-dark);
+  color: var(--color-primary-dark);
 }
 
 .carousel__nav-dots {
   display: flex;
   justify-content: center;
+  align-items: center;
   gap: 0.5rem;
   margin-top: 1rem;
 }
