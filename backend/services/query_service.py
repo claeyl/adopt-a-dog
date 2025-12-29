2cea4f8400c265c8948aab0d71d1d56fa7010eb5
Calley Chai <annyi.chai@gmail.com>
2025-12-29 22:17:52 +0800

    feat: adding constants and return metadata from query

--- backend/services/query_service.py
+++ backend/services/query_service.py
@@ -1,13 +1,17 @@
 import os
+import logging
 import weaviate
 
 from dotenv import load_dotenv
-from weaviate.classes.query import MetadataQuery, Rerank
+from weaviate.classes.query import Rerank, MetadataQuery
 from backend.services.filter_service import build_filter_expression, construct_json_filters_from_prompt
 
+logger = logging.getLogger(__name__)
 
+OVERFETCH_LIMIT = 20
 RESULT_SIZE = 5
 
+# This returns a list of Weaviate's QueryReturn type, but it is not exposed for public use, so type can't be annotated
 def query_collection(query: str):
   load_dotenv()
   cohere_key = os.getenv("COHERE_API_KEY")
@@ -15,7 +19,7 @@ def query_collection(query: str):
     raise Exception("Cohere key does not exist")
   
   headers = { "X-Cohere-Api-Key": cohere_key }
-  client = weaviate.connect_to_local(headers=headers)
+  client = weaviate.connect_to_local(headers=headers) # TODO: eventually change this to connect_to_weaviate_cloud
   
   try:
     dogs = client.collections.use("Dog")
@@ -26,13 +30,17 @@ def query_collection(query: str):
     response = dogs.query.near_text(
       query=query,
       filters=(filters),
+      limit=OVERFETCH_LIMIT,
       rerank=Rerank(
         prop="description",
         query=query
       ),
-      limit=RESULT_SIZE,
+      return_metadata=MetadataQuery(explain_score=True)
     )
-    return response
+
+    if not response.objects:
+      return None
+    return response.objects[:RESULT_SIZE]
   except Exception as err:
     print(err)
     raise err
