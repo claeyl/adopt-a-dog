4d0d9c5fa6abd41b6f5ceb3de1ccc4c6c6f2e9c0
Calley Chai <annyi.chai@gmail.com>
2025-12-29 22:05:50 +0800

    feat: convert snake to camel case for api models

--- backend/models/dtos/DogResponseInfo.py
+++ backend/models/dtos/DogResponseInfo.py
@@ -1,9 +1,13 @@
-from dataclasses import dataclass
 from typing import List
+from pydantic import BaseModel
+from pydantic.alias_generators import to_camel
 
 
-@dataclass
-class DogResponseInfo:
+class DogResponseInfo(BaseModel):
+  class Config:
+    alias_generator = to_camel
+    populate_by_name = True
+  
   id: int
   name: str
   gender: str
