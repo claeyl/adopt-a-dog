3e89e0c8c92ac2637ed10d77b380a8cabe02fbb6
Calley Chai <annyi.chai@gmail.com>
2025-12-30 13:17:43 +0800

    feat: added a factory method and str method

--- backend/models/Dog.py
+++ backend/models/Dog.py
@@ -16,3 +16,18 @@ class Dog:
   tags: List[str]
   description: str
   filters: DogFilters
+  
+  @classmethod
+  def from_weaviate(cls, dog_id: int, props: dict) -> "Dog":
+    filters = DogFilters.from_weaviate(props)
+    
+    field_names = [f for f in cls.__dataclass_fields__.keys() if f != "filters"]
+    dog_kwargs = {k: props[k] for k in field_names if k in props}
+
+    dog_kwargs["id"] = dog_id
+    dog_kwargs["filters"] = filters
+    return cls(**dog_kwargs)
+
+
+  def __str__(self) -> str:
+    return f"{self.name} is a {self.gender} {self.breed}, {self.age} years old, {self.size} size. It has these tags: {self.tags}. {self.description}"
