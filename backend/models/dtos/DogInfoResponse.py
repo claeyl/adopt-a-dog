0db318e1f21d0cd5f1cfadcc89d6b594ad8d5503
Calley Chai <annyi.chai@gmail.com>
2025-12-30 12:22:01 +0800

    feat: update response def

---
+++ backend/models/dtos/DogInfoResponse.py
@@ -0,0 +1,21 @@
+from typing import List
+from pydantic import BaseModel
+from pydantic.alias_generators import to_camel
+
+
+class DogInfoResponse(BaseModel):
+  class Config:
+    alias_generator = to_camel
+    populate_by_name = True
+  
+  id: int
+  name: str
+  gender: str
+  age: float
+  breed: str
+  size: str
+  weight: float
+  adoption_fee: float
+  tags: List[str]
+  description: str
+  explanation: str
