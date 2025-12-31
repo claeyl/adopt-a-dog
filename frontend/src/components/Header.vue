65c80b3c1d374d3a23ebe5881f9620b95af70361
Calley Chai <annyi.chai@gmail.com>
2025-12-31 20:56:33 +0800

    feat: app header

---
+++ frontend/src/components/Header.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+const emit = defineEmits<{
+  (e: 'newSearch'): void
+}>()
+</script>
+<template>
+  <header class="fixed inset-0 z-10 p-3 h-15 border-b-1 border-surface-30 bg-surface-0">
+    <button
+      class="flex items-center gap-2 hover:opacity-80 transition-opacity"
+      @click="() => emit('newSearch')"
+    >
+      <img src="../assets/logo.svg" alt="logo" class="w-9 aspect-square" />
+    </button>
+  </header>
+</template>
