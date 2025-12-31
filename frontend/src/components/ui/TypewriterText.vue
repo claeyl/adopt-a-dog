ebb5fadb45cb998b7e9546cec7ce4350eecd1f06
Calley Chai <annyi.chai@gmail.com>
2026-01-01 01:09:50 +0800

    refactor: use tailwind classes

---
+++ frontend/src/components/ui/TypewriterText.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+defineProps<{
+  content: string
+}>()
+</script>
+
+<template>
+  <div class="w-max">
+    <p class="w-0 overflow-hidden whitespace-nowrap border-r typing-effect">{{ content }}</p>
+  </div>
+</template>
+
+<style scoped>
+.typing-effect {
+  animation: typing 2s forwards;
+}
+
+@keyframes typing {
+  from {
+    width: 0;
+  }
+  to {
+    width: 100%;
+    border-right: 0;
+  }
+}
+</style>
