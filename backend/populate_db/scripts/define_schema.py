1fc922ddbc2e79a907f89a41a727e32892d338c4
Calley Chai <annyi.chai@gmail.com>
2026-01-02 17:32:58 +0800

    feat: update definition of dog collection schema

--- backend/populate_db/scripts/define_schema.py
+++ backend/populate_db/scripts/define_schema.py
@@ -16,53 +16,60 @@ try:
     raise Exception("Cohere key does not exist")
   
   headers = { "X-Cohere-Api-Key": cohere_key }
-  client = weaviate.connect_to_local(headers=headers)
+  client = weaviate.connect_to_local()
   
   dogs = client.collections.create(
   name="Dog",
   description="Collection of dogs available for adoption",
-  vector_config=Configure.Vectors.text2vec_cohere(source_properties=["tags", "description"]),
-  reranker_config=Configure.Reranker.cohere(),
+  # vector_config=Configure.Vectors.text2vec_cohere(source_properties=["tags", "description"]),
+  # reranker_config=Configure.Reranker.cohere(),
+  vector_config=Configure.Vectors.text2vec_transformers(source_properties=["tags", "description"]),
+  reranker_config=Configure.Reranker.transformers(),
   properties=[
     Property(
       name="dog_id",
       data_type=DataType.INT,
-      vectorize_property_name=False,
+      skip_vectorization=True,
     ),
     Property(
       name="name",
       data_type=DataType.TEXT,
-      vectorize_property_name=False,
+      skip_vectorization=True,
+    ),
+    Property(
+      name="image_url",
+      data_type=DataType.TEXT,
+      skip_vectorization=True,
     ),
     Property(
       name="gender",
       data_type=DataType.TEXT,
-      vectorize_property_name=False,
+      skip_vectorization=True,
     ),
     Property(
       name="age",
       data_type=DataType.INT,
-      vectorize_property_name=False,
+      skip_vectorization=True,
     ),
     Property(
       name="breed",
       data_type=DataType.TEXT,
-      vectorize_property_name=False,
+      skip_vectorization=True,
     ),
     Property(
       name="size",
       data_type=DataType.TEXT,
-      vectorize_property_name=False,
+      skip_vectorization=True,
     ),
     Property(
       name="weight",
       data_type=DataType.NUMBER,
-      vectorize_property_name=False,
+      skip_vectorization=True,
     ),
     Property(
       name="adoption_fee",
       data_type=DataType.NUMBER,
-      vectorize_property_name=False,
+      skip_vectorization=True,
     ),
     Property(
       name="tags",
@@ -80,47 +87,47 @@ try:
     Property(
       name="friendly_with_cats",
       data_type=DataType.INT,
-      vectorize_property_name=False,
+      skip_vectorization=True,
     ),
     Property(
       name="friendly_with_dogs",
       data_type=DataType.INT,
-      vectorize_property_name=False,
+      skip_vectorization=True,
     ),
     Property(
       name="single_dog_household",
       data_type=DataType.BOOL,
-      vectorize_property_name=False,
+      skip_vectorization=True,
     ),
     Property(
       name="suitable_for_fulltime_workers",
       data_type=DataType.INT,
-      vectorize_property_name=False,
+      skip_vectorization=True,
     ),
     Property(
       name="behaviour_training_needed",
       data_type=DataType.BOOL,
-      vectorize_property_name=False,
+      skip_vectorization=True,
     ),
     Property(
       name="experienced_dog_owners_needed",
       data_type=DataType.BOOL,
-      vectorize_property_name=False,
+      skip_vectorization=True,
     ),
     Property(
       name="can_live_with_children",
       data_type=DataType.TEXT,
-      vectorize_property_name=False,
+      skip_vectorization=True,
     ),
     Property(
       name="medical_needs",
       data_type=DataType.BOOL,
-      vectorize_property_name=False,
+      skip_vectorization=True,
     ),
     Property(
       name="calm_home_needed",
       data_type=DataType.BOOL,
-      vectorize_property_name=False,
+      skip_vectorization=True,
     ),
   ]
 )
