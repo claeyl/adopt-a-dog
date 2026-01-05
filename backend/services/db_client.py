1143a075506151df3a5aaa68819ca742f9b7d3a1
Calley Chai <annyi.chai@gmail.com>
2026-01-05 18:59:35 +0800

    feat: update on how to run locally

--- backend/services/db_client.py
+++ backend/services/db_client.py
@@ -15,6 +15,16 @@ The client will also be closed in the caller function
 
 def create_db_client() -> WeaviateClient:
   load_dotenv()
+  
+  connect_to_local = os.getenv("RUN_LOCAL", "true")
+  model = os.getenv("MODEL", "t2v")
+  
+  if connect_to_local == "true":
+    if model == "t2v":
+      return _create_db_client_local_t2v()
+    else:
+      return _create_db_client_local_cohere()
+  
   cohere_key = os.getenv("COHERE_API_KEY")
   weaviate_url = os.environ["WEAVIATE_CLUSTER_URL"]
   weaviate_key = os.environ["WEAVIATE_API_KEY"]
@@ -36,18 +46,26 @@ def create_db_client() -> WeaviateClient:
     }
   )
   logger.info(f"Connected to weaviate: {client.is_ready()}")
+  client = _create_db_client_local_t2v()
   
   return client
 
-# If using local weaviate instance
-def _create_db_client() -> WeaviateClient:
+# If using local weaviate instance with Cohere
+def _create_db_client_local_cohere() -> WeaviateClient:
   load_dotenv()
   cohere_key = os.getenv("COHERE_API_KEY")
   if not cohere_key:
     raise Exception("Cohere key does not exist")
   
   headers = { "X-Cohere-Api-Key": cohere_key }
-  client = weaviate.connect_to_local(headers=headers)
+  client = weaviate.connect_to_local(headers=headers, host="weaviate")
+  logger.info(f"Connected to weaviate: {client.is_ready()}")
+  
+  return client
+
+# If using local weaviate instance with t2v
+def _create_db_client_local_t2v() -> WeaviateClient:
+  client = weaviate.connect_to_local(host="weaviate")
   logger.info(f"Connected to weaviate: {client.is_ready()}")
   
   return client
